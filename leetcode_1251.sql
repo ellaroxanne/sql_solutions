87b258d3beb5a1210cfe7aa913f5f9ba4d33baeb
Ella Young <136139560+ellaroxanne@users.noreply.github.com>
2024-01-08 15:06:21 -0800

    Create leetcode_1251.sql

---
+++ leetcode_1251.sql
@@ -0,0 +1,3 @@
+SELECT Prices.product_id, IFNULL(ROUND(SUM(Prices.price * UnitsSold.units)/SUM(UnitsSold.units), 2), 0) AS average_price
+FROM Prices LEFT JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id AND UnitsSold.purchase_date BETWEEN Prices.Start_date and Prices.end_date
+GROUP BY Prices.product_id;
