278363db74a462aaee5faab9e46c9fc71eb2973a
Ella Young <136139560+ellaroxanne@users.noreply.github.com>
2024-01-19 14:57:59 -0800

    Update leetcode_1070.sql

--- leetcode_1070.sql
+++ leetcode_1070.sql
@@ -1,3 +1,8 @@
-SELECT DISTINCT product_id, MIN(year) AS first_year, quantity, price
+SELECT product_id, MIN(year) AS first_year, SUM(quantity) AS quantity, price
 FROM Sales
-GROUP BY product_id;
+WHERE (product_id, year) IN (
+  SELECT product_id, MIN(year) AS min_year 
+  FROM Sales
+  GROUP BY product_id)
+GROUP BY
+    product_id, price;
